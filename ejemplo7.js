80e1c1514b5f88767594bebee49dc52d6874f568
juan <juan@prueba.com>
2025-07-23 14:23:37 +0200

    ejemplo7

--- ejemplo7.js
+++ ejemplo7.js
@@ -2,13 +2,15 @@ function comprobar(evt) {
     const nombre = document.getElementById("nombre");
     const edad = document.getElementById("edad");
     const clave = document.getElementById("clave");
+    const repetirClave = document.getElementById("repetirClave");
+    const provincia = document.getElementById("provincia");
 
     // Para no ir haciendo elses para quitar los posibles fondos rojos
     // podríamos hacer esto
     /*document.getElementsByClassName("bg-danger-subtle")
         .forEach( e => e.classList.remove("bg-danger-subtle"));
     */
-   
+
     let errores = []; // Creamos un array vacío
 
     // Con el método checkValidity podemos comprobar si un elemento 
@@ -38,9 +40,36 @@ function comprobar(evt) {
         clave.classList.remove("bg-danger-subtle");
         //clave.style.backgroundColor = "inherit";
     }
-    if(errores != "") { // Vemos si errores tiene algo
-        alert(errores);
+    if(repetirClave.value != clave.value) {
+        errores.push("Las claves no coinciden");
+        repetirClave.classList.add("bg-danger-subtle");
+        //clave.style.backgroundColor = "lightsalmon";
+    } else {
+        repetirClave.classList.remove("bg-danger-subtle");
+        //repetirClave.style.backgroundColor = "inherit";
+    }
+    if(provincia.selectedIndex == -1) {
+        errores.push("Selecciona la provincia");
+        // Hoy día los navegadores no ponen en rojo el select
+        provincia.classList.add("bg-danger-subtle");
+        //provincia.style.backgroundColor = "lightsalmon";
+    } else {
+        provincia.classList.remove("bg-danger-subtle");
+        //provincia.style.backgroundColor = "inherit";
+    }
+    
+    const ul = document.getElementById("errores");
+    if(errores != []) { // Vemos si errores tiene algo
         evt.preventDefault();
+        ul.classList.remove("d-none");
+        ul.textContent = "";
+        for (const error of errores) {
+            const li = document.createElement("li");
+            ul.appendChild(li);
+            li.textContent = error;
+        }
+    } else {
+        ul.classList.add("d-none");
     }
 }
 document.getElementById("formulario")
