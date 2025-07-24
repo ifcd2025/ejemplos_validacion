67b2efc8e464484a8a05f2fd9cb43684f74ab8bd
juan <juan@prueba.com>
2025-07-24 10:07:07 +0200

    validaciones

--- validaciones.js
+++ validaciones.js
@@ -1,3 +1,8 @@
+/**
+ * Comprueba si un texto está vacío
+ * @param {string} valor 
+ * @returns {boolean} true si está vacío, false si no
+ */
 function isVacio(valor) {
     if(valor.trim() === "") {
         return true;
@@ -7,6 +12,12 @@ function isVacio(valor) {
     //return valor.trim() === "";
 }
 
+/**
+ * Comprueba si un texto tiene una longitud mínima
+ * @param {string} valor - el texto a comprobar
+ * @param {number} minimo - la longitud mínima
+ * @returns {boolean} - true si tiene la longitud mínima, false si no
+ */
 function isLongitudMinima(valor, minimo) {
     if(valor.trim().length < minimo) {
         return false;
@@ -55,5 +66,31 @@ function isFecha(valor) {
     if(/^(\d{1,2}[-\/]){2}\d{4}$/.test(valor) == false) {
         return false;
     }
+    valor = valor.replace("-", "/");
+    let [dia, mes, año] = valor.split("/");
 
+    // - 1 ya que enero es el mes 0
+    const fecha = new Date(año, mes - 1, dia);
+    if(isNaN(fecha.getTime())) {
+        return false;
+    }
+    // Si creamos el objeto con días de más, no da error, y lo que hace es
+    // pasar al siguiente mes y puede que también al siguiente año
+    if(fecha.getFullYear() == año 
+        && fecha.getMonth() == mes - 1 
+        && fecha.getDate() == dia) { // getDate es el día del mes
+            return true;
+        }
+    else {
+        return false;
+    }
+}
+
+function isRadiosMarcado(nombre) {
+    document.getElementsByTagName(nombre).forEach(radio => {
+        if(radio.checked) {
+            return true;
+        }    
+    });
+    return false;
 }
